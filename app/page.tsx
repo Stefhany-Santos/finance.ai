e73382206b5fa2a6b34d4c1326cbb663339116ee
Stefhany Santos <stefhany.contato@outlook.com>
2026-04-13 07:46:56 -0300

    chore: adiciona o plugin do tailwind prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 const Home = () => {
   return (
     <div className="home">
-      <h2>Welcome</h2>
-      <p>This is the home page</p>
+                  <h2 className="text-red-500 flex">Welcome</h2>
+                    <p>This is the home page</p>
     </div>
   );
 };
